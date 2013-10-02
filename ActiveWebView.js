e036b24bb54d898646428d81d9c2e01b81635beb
Hector Vergara <hvergara@gmail.com>
2013-10-02 03:25:24 -0300

    Added JS bridge

---
+++ ActiveWebView.js
@@ -0,0 +1,21 @@
+
+var ActiveWebView = {
+	
+  call: function(method, args) {
+
+	 var payload = JSON.stringify({
+     arguments: args || []
+    })
+  
+    var url = 'native://' + method + '/' + encodeURIComponent(message)
+      , frame = document.createElement('iframe')
+
+    frame.src = url
+    frame.style.display = 'none'
+    
+    document.body.appendChild(iframe)
+    iframe.parentNode.removeChild(iframe)
+    
+  }
+
+}
